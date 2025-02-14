1788353c05fc69f890182e3219e146decede348a
Eliya kaheni <89583292+EliyaKaheni@users.noreply.github.com>
2025-02-14 19:13:33 +0330

    total price added & customer clustering created.

--- pages/ordering.py
+++ pages/ordering.py
@@ -4,22 +4,23 @@ import numpy as np
 import datetime
 import json
 
+
 # Load products from JSON file
 with open('pages/pages_utils/products.json') as file:
     products = json.load(file)
 
-# Initialize session state for orders if it doesn't exist
+# Initial settings
+st.set_page_config(layout="wide")
 if "orders" not in st.session_state:
     st.session_state.orders = pl.DataFrame()
 
-# Sidebar for user input
 with st.sidebar:
     customer_id = st.text_input(label='آیدی مشتری')
     product = st.selectbox('محصولات', list(products.keys()))
     quantity = st.number_input('تعداد', step=1)
 
+    # Adding new order
     if st.button('افزودن'):
-        # Create a new order DataFrame
         new_order = pl.DataFrame({
             'تاریخ': [datetime.datetime.today().strftime('%Y-%m-%d')],
             'ساعت': [datetime.datetime.today().hour],
@@ -30,11 +31,31 @@ with st.sidebar:
             'آیدی مشتری': [customer_id]
         })
 
-        # Update session state orders DataFrame
         st.session_state.orders = pl.concat([st.session_state.orders, new_order])
+    
+    # Deleting the last row
+    if st.button('حذف آخرین ردیف'):
+        if not st.session_state.orders.is_empty():
+            st.session_state.orders = st.session_state.orders.slice(0, -1)
 
-# Display the orders if there are any
+# Displaying the orders
 if not st.session_state.orders.is_empty():
-    st.dataframe(st.session_state.orders, width=200, use_container_width=True)
+    total_quantity = st.session_state.orders['تعداد'].sum()
+    total_price = (st.session_state.orders['تعداد'] * st.session_state.orders['قیمت واحد']).sum()
+
+    summation_row = pl.DataFrame({
+        'تاریخ': [datetime.datetime.today().strftime('%Y-%m-%d')],
+        'ساعت': [datetime.datetime.today().hour],
+        'محصول': ['جمع کل سفارش'],
+        'آیدی فروشگاه': [0],
+        'تعداد': [total_quantity],
+        'قیمت واحد': [total_price],
+        'آیدی مشتری': [customer_id]
+    })
+
+    orders_with_sum = pl.concat([summation_row, st.session_state.orders])
+
+    st.dataframe(orders_with_sum, width=1000, use_container_width=True)
+
 else:
-    st.write("No orders yet.")
+    st.write("سفارشی ثبت نشده است.")
---
+++ services/customer_clustering.py
@@ -0,0 +1,4 @@
+import polars as pl
+from sklearn.mixture import GaussianMixture
+import plotly.express as px
+
